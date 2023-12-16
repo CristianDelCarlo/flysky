9a22b125e8e97985de7288689e126f226f903394
CristianDelCarlo <cristiandelcarlo@yahoo.com.ar>
2023-12-15 23:43:27 -0300

    Aplicación andando, base no se crea eb MySQL

--- src/main/java/com/example/flysky/FlyskyApplication.java
+++ src/main/java/com/example/flysky/FlyskyApplication.java
@@ -3,11 +3,15 @@ package com.example.flysky;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FlyskyApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlyskyApplication.class, args);
 	}
+
+
 
 }
